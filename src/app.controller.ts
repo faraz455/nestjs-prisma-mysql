2be0d97c2caee20a802bd5ee5337f31180d0175b
Faraz Khan <faraz@cognitivehealthintl.com>
2024-02-26 17:13:13 +0500

    Remove unused controller end points from app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,20 +7,6 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  @Render('index')
-  root() {
-    return { message: 'Static view!' };
-  }
-
-  @Get('dynamic_render')
-  dynamic_render(@Res() res: Response) {
-    return res.render(this.appService.getViewName(), {
-      message: 'Dynamic view!',
-    });
-  }
-
   @Get('health')
   healthCheck() {
     return 'Ok';
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-
-import { CommonService } from './common/common.service';
 
 @Injectable()
 export class AppService {
   constructor() {}
-
-  getViewName(): string {
-    return 'index';
-  }
 }
