21d7f13cdfd6a8a678d20ad629f9f2262de58787
faraz khan <khanfaraz1199@gmail.com>
2024-08-04 05:45:19 -0500

    Add permission decorators in articles controller

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -24,38 +24,46 @@ import { CreateArticleDto, UpdateArticleDto } from './dto';
 import { ArticleEntity } from './entities';
 
 import { CustomJwtGuard } from 'src/auth/guards/custom-jwt.guard';
+import { PermissionsGuard } from 'src/auth/guards/permissions.guard';
+import { Permissions } from 'src/auth/decorators';
 
 @UseGuards(CustomJwtGuard)
+@UseGuards(PermissionsGuard)
 @ApiTags('Articles')
 @ApiBearerAuth()
 @Controller('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
+  @Permissions({ AND: ['Article.create'] })
   @ApiCreatedResponse({ type: IDDto })
   @Post()
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(createArticleDto);
   }
 
+  @Permissions({ AND: ['Article.view'] })
   @ApiPaginatedResponse({ status: 200, type: ArticleEntity })
   @Get()
   findAll(@Query() query: PaginationQueryDto) {
     return this.articlesService.findAll(query);
   }
 
+  @Permissions({ AND: ['Article.view'] })
   @ApiOkResponse({ type: ArticleEntity })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.articlesService.findOne(id);
   }
 
+  @Permissions({ AND: ['Article.update'] })
   @ApiOkResponse({ type: IDDto })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articlesService.update(id, updateArticleDto);
   }
 
+  @Permissions({ AND: ['Article.delete'] })
   @ApiOkResponse({ type: IDDto })
   @Delete(':id')
   remove(@Param('id') id: string) {
