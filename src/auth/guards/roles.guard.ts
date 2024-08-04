3820016a79ffe8134af8a448f98805e14270a9e2
faraz khan <khanfaraz1199@gmail.com>
2024-08-04 07:40:39 -0500

    Use IsPublic decorator in authentication

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -5,13 +5,21 @@ import {
   ROLES_KEY,
   RolesObject,
 } from '../decorators/roles.decorator';
-import { Profile } from '../dto';
+import { IS_PUBLIC_KEY } from '../decorators';
 
 @Injectable()
 export class RolesGuards implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const requiredRoles = this.reflector.getAllAndOverride<RolesObject[]>(
       ROLES_KEY,
       [context.getHandler(), context.getClass()],
