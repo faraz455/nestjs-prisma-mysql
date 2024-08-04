4fe0e45a74dd239b173483ac2f5e57c436aa7f9b
faraz khan <khanfaraz1199@gmail.com>
2024-08-04 05:41:20 -0500

    Fix custom string decorator

--- src/common/decorators/is-custom-string.decorator.ts
+++ src/common/decorators/is-custom-string.decorator.ts
@@ -3,15 +3,10 @@ import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 function buildOptionsWithDefaults(options: StringOptions) {
-  const usedOptions = new StringOptions();
-  if (options) {
-    let key: keyof StringOptions;
-    for (key in usedOptions) {
-      if (options[key]) {
-        usedOptions[key] = options[key];
-      }
-    }
-  }
+  const usedOptions: StringOptions = {
+    required: true,
+    ...options,
+  };
   return usedOptions;
 }
 
@@ -20,21 +15,20 @@ export class StringOptions {
 }
 
 /**
- * Verify that the property decorated by this is a string that
- * matches the expectations of the CHI ecosystem.
+ * Verify that the property decorated by this is a string
  *
  * If optional, empty string will **ALWAYS** be transformed to `null`.
  *
- * @param optional - Apply the IsOptional validator, default `false`
- * default `false`
+ * @param optional - Apply the required validator, default `true`
  */
 export const IsCustomString = (options?: StringOptions) => {
   options = buildOptionsWithDefaults(options);
 
   let decorators: PropertyDecorator[] = [IsString(), IsNotEmpty()];
+  decorators.push(Transform(({ value }) => (value != '' ? value : null)));
+
   if (!options.required) {
     decorators.push(IsOptional());
-    decorators.push(Transform(({ value }) => (value != '' ? value : null)));
   }
 
   return applyDecorators(...decorators);
